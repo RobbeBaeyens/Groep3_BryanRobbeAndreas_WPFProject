20fdcd895313bce83037dcc18e886d32beaa0874
godofsound <robbe.baeyens@outlook.com>
2020-06-16 15:33:16 +0200

    event

---
+++ GRP3_Project/GRP3_Project_DAL/DatabaseOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace GRP3_Project_DAL
+{
+    public class DatabaseOperations
+    {
+        /*=====================
+         * EVENT
+         =====================*/
+        //ophalen
+        public static List<Event> OphalenEvents()
+        {
+            using (EventBeheerEntities entities = new EventBeheerEntities())
+            {
+                var query = entities.Event
+                    .Include(x => x.Eventtype)
+                    .OrderBy(x => x.EventID);
+                return query.ToList();
+            }
+        }
+    }
+}
