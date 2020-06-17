dd5f0b636a3f38fcd7477757464def386bee0c6a
Bryan Antonis <bryan-antonis@hotmail.com>
2020-06-17 20:55:33 +0200

    Addlocation code toegevoegd

--- GRP3_Project/GRP3_Project_WPF/LocationAdd.xaml.cs
+++ GRP3_Project/GRP3_Project_WPF/LocationAdd.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -43,14 +44,58 @@ namespace GRP3_Project_WPF
 
         private void btnSaveLocation_Click(object sender, RoutedEventArgs e)
         {
-            locAdd.name = txtName.Text;
-            locAdd.street = txtStreet.Text;
-            locAdd.number = int.Parse(txtNumber.Text);
-            locAdd.postal = int.Parse(txtPostal.Text);
-            locAdd.city = txtCity.Text;
-            locAdd.manager = txtManager.Text;
-            locAdd.email = txtEmail.Text;
-            locAdd.phone = txtPhone.Text;
+            string foutmeldingen = Valideer("Huisnr");
+            foutmeldingen += Valideer("Postcode");
+
+            if (string.IsNullOrWhiteSpace(foutmeldingen))
+            {
+                Locatie locatie = new Locatie();
+
+                locatie.Naam = txtName.Text;
+                locatie.Straat = txtStreet.Text;
+                locatie.Gemeente = txtCity.Text;
+                locatie.Manager = txtManager.Text;
+                locatie.Email = txtEmail.Text;
+                locatie.Telefoon = txtPhone.Text;
+
+                if (int.TryParse(txtNumber.Text, out int huisnr))
+                {
+                    locatie.Huisnr = huisnr;
+                }
+
+                if (int.TryParse(txtPostal.Text, out int postcode))
+                {
+                    locatie.Postcode = postcode;
+                }
+
+                if (locatie.IsGeldig())
+                {
+                    DatabaseOperations.ToevoegenLocatie(locatie);
+
+                    btnBackToLocationSelector_Click(sender, e);
+                }
+                else
+                {
+                    MessageBox.Show(locatie.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show(foutmeldingen);
+            }
+        }
+
+        private string Valideer(string input)
+        {
+            if (input == "Huisnr" && (!int.TryParse(txtNumber.Text, out int huisnr) || string.IsNullOrWhiteSpace(txtNumber.Text)))
+            {
+                return "Vul een correct huisnummer in!" + Environment.NewLine; ;
+            }
+            if (input == "Postcode" && (!int.TryParse(txtPostal.Text, out int postcode) || string.IsNullOrWhiteSpace(txtPostal.Text)))
+            {
+                return "Vul een correcte postcode in!" + Environment.NewLine;
+            }
+            return "";
         }
     }
 }
