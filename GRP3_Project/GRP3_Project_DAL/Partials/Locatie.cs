a4d6263548bc07b2836706058419dec6c1af7367
Bryan Antonis <bryan-antonis@hotmail.com>
2020-06-18 16:49:51 +0200

    Schermen en commentaar af

--- GRP3_Project/GRP3_Project_DAL/Partials/Locatie.cs
+++ GRP3_Project/GRP3_Project_DAL/Partials/Locatie.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using GRP3_Project_MODEL;
 
-//Bryan Antonis
+//BRYAN ANTONIS
 
 namespace GRP3_Project_DAL
 {
@@ -16,6 +16,7 @@ namespace GRP3_Project_DAL
         {
             get
             {
+                //Check of Naam, Straat, Gemeente, Manager, Email, Telefoon leeg is en return een String voor validatie
                 if (columnName == "Naam" && string.IsNullOrWhiteSpace(Naam))
                 {
                     return "Vul een locatie naam in!";
@@ -25,16 +26,6 @@ namespace GRP3_Project_DAL
                 {
                     return "Vul een straat in!";
                 }
-
-                //if (columnName == "Huisnr" && Huisnr.GetType() != typeof(int))
-                //{
-                //    return "Vul een huisnummer in!";
-                //}
-
-                //if (columnName == "Postcode" && Postcode.GetType() != typeof(int))
-                //{
-                //    return "Vul een postcode in!";
-                //}
 
                 if (columnName == "Gemeente" && string.IsNullOrWhiteSpace(Gemeente))
                 {
@@ -46,6 +37,7 @@ namespace GRP3_Project_DAL
                     return "Vul een manager in!";
                 }
 
+                //Regex voor validatie van Email
                 if (columnName == "Email" && (string.IsNullOrWhiteSpace(Email) || 
                     !Regex.IsMatch(Email, @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
                 @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$")))
